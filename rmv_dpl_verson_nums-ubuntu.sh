1929b5fc4a55b976a1e7818f1ac5c0249d03c086
Maria McDowell <maria.mcdowell@acquia.com>
2012-12-27 17:34:51 -0800

    MGM: remove version numbers now work for simpletest modules, ubuntu.  PF patches.

---
+++ rmv_dpl_verson_nums-ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/tcsh
+# Script removes drupal packing script info from core module and theme info files.
+# run on older version of drupal, BEFORE applying patches.
+# This does not remove Acquia Drupal packing information.
+#
+# must: chmod +x ~/rmv_dpl-version_nums.sh (makes executable)
+#
+# - @mariagwyn with the  help of greggles.
+#
+sed -i='' 's/; Information added by drupal.org packaging script on [0-9]*-[0-9]*-[0-9]*//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info
+sed -i='' 's/version = "[0-9]*.[0-9]*"//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info
+sed -i='' 's/project = "drupal"//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info
+sed -i='' 's/datestamp = "[0-9]*"//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info
+sed -i='' '/^$/d' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info
+echo "Version numbers removed"
