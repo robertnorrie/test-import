23b80f475518e5be904c7e4aff2ca2fbd64441fd
Maria McDowell <maria.mcdowell@acquia.com>
2013-01-30 15:00:55 -0800

    MGM: added eliza411's modifications to versions scripts.

--- rmv_dpl-version_nums.sh
+++ rmv_dpl-version_nums.sh
@@ -1,14 +1,27 @@
-#Script removes drupal packing script info from core module and theme info files.
+#!/bin/bash
+#
+# Script removes drupal packing script info from core module and theme info files.
 # run on older version of drupal, BEFORE applying patches.
 # This does not remove Acquia Drupal packing information.
 #
 # must: chmod +x ~/rmv_dpl-version_nums.sh (makes executable)
 #
-# - @mariagwyn with the  help of greggles.
+# - @mariagwyn with the help of greggles; significantly cleaned up by eliza411.
 #
-sed -i '' 's/; Information added by drupal.org packaging script on [0-9]*-[0-9]*-[0-9]*//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info modules/*/*/*/*/*/*.info profiles/*/*/*/*.info profiles/*/*/*/*/*.info profiles/*/*.info
-sed -i '' 's/version = "[0-9]*.[0-9]*"//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info modules/*/*/*/*/*/*.info profiles/*/*/*/*.info profiles/*/*/*/*/*.info profiles/*/*.info
-sed -i '' 's/project = "drupal"//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info modules/*/*/*/*/*/*.info profiles/*/*/*/*.info profiles/*/*/*/*/*.info profiles/*/*.info
-sed -i '' 's/datestamp = "[0-9]*"//g' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info modules/*/*/*/*/*/*.info profiles/*/*/*/*.info profiles/*/*/*/*/*.info profiles/*/*.info
-sed -i '' '/^$/d' modules/*/*.info themes/*/*.info themes/tests/*/*.info modules/*/*/*.info modules/*/*/*/*.info modules/*/*/*/*/*.info modules/*/*/*/*/*/*.info profiles/*/*/*/*.info profiles/*/*/*/*/*.info profiles/*/*.info
-echo "Version numbers removed"
+
+# Specifies filetype to modify
+FILETYPE='*.info'
+
+# Matches recursively only in the specfied directories
+ INCLUDEONLY='modules/ themes/ profiles/'
+ files=( $(find $INCLUDEONLY -name "$FILETYPE") )
+
+# Deletes these four lines and print the path and file name for each file
+for file in "${files[@]}"; do
+  sed -i '' '/; Information added by drupal.org packaging script on [0-9]*-[0-9]*-[0-9]*/d' $file
+  sed -i '' '/version = "[0-9]*.[0-9]*"/d' $file
+  sed -i '' '/project = "drupal"/d' $file
+  sed -i '' '/datestamp = "[0-9]*"/d' $file
+  echo 'Checking' $file
+done
+  echo 'Checked' ${#files[@]} 'files and removed drupal.org packaging data'
